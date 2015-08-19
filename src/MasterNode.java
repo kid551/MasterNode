c22bc7beb26a68e558e7c545e2f6840e0a9eba12
Tao Xie <xietao26@gmail.com>
2015-08-19 09:57:13 +0800

    Src

---
+++ src/MasterNode.java
@@ -0,0 +1,15 @@
+
+public class MasterNode {
+
+	public String nodeTag = "Master Node";
+	
+	public void nodeInfo() {		
+		System.out.println("Node num = " + nodeTag);
+	}
+	
+	public static void main(String[] args) {
+		SlaverNode sn = new SlaverNode();
+		System.out.println("Node info = " + sn.nodeTag);
+	}
+
+}
